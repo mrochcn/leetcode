bee7e22911aacca75bf32a5b38dbea088bffec11
mrochcn <mrochcn@gmail.com>
2021-05-23 16:00:30 +0800

    LinkedHashMap

---
+++ src/main/java/com/cuizx/design/LRUCache46.java
@@ -0,0 +1,28 @@
+package com.cuizx.design;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCache46 extends LinkedHashMap<Integer, Integer>{
+
+    private int capacity;
+
+    public LRUCache46(int capacity) {
+        super(capacity, 0.75F, true);
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
+    }
+}
