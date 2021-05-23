c0eb8af45c95c82f531bf7370ac48fa2e7845fd3
mrochcn <mrochcn@gmail.com>
2021-05-23 14:18:25 +0800

    merge 88

---
+++ src/main/java/com/cuizx/arrays/Merge88.java
@@ -0,0 +1,13 @@
+package com.cuizx.arrays;
+
+import java.util.Arrays;
+
+public class Merge88 {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = m; i < m + n; i++) {
+            nums1[i] = nums2[i - m];
+        }
+        Arrays.sort(nums1);
+    }
+}
