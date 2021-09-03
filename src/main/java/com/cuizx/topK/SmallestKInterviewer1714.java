e4459d4c15686fe0f9dd97dd7377931086b3cc45
cuizxa <cuizxa@yonyou.com>
2021-09-03 16:35:39 +0800

    update repository

---
+++ src/main/java/com/cuizx/topK/SmallestKInterviewer1714.java
@@ -0,0 +1,25 @@
+package com.cuizx.topK;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
+public class SmallestKInterviewer1714 {
+
+    public int[] smallestK(int[] arr, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 - o2;
+            }
+        });
+        for (int i : arr) {
+            queue.offer(i);
+        }
+        int[] ans = new int[k];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = queue.poll();
+        }
+        return ans;
+    }
+}
