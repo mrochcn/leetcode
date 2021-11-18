79789d0c04268367b26b9e06bfb7b20b08c28bce
cuizxa <cuizxa@yonyou.com>
2021-11-18 14:27:25 +0800

    update repository

--- src/main/java/com/cuizx/algorithm/SortedSquares977.java
+++ src/main/java/com/cuizx/algorithm/SortedSquares977.java
@@ -1,2 +1,17 @@
-package com.cuizx.algorithm;public class SortedSquares977 {
+package com.cuizx.algorithm;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class SortedSquares977 {
+
+//    //暴力
+//    public int[] sortedSquares(int[] nums) {
+//
+//    }
+    //双指针
+    public int[] sortedSquares(int[] nums) {
+
+        return new int[]{0, 1};
+    }
 }
--- src/main/java/com/cuizx/binaryTree/FindTilt563.java
+++ src/main/java/com/cuizx/binaryTree/FindTilt563.java
@@ -1,2 +1,36 @@
-package com.cuizx.binaryTree;public class FindTilt563 {
+package com.cuizx.binaryTree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class FindTilt563 {
+
+    public int findTilt(TreeNode root) {
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+
+            for (int i = 0; i < n; i++) {
+                TreeNode tn = queue.poll();
+                if (tn.left != null && tn.right != null) {
+                    ans = ans + Math.abs(tn.left.val - tn.right.val);
+                } else if (tn.left == null && tn.right == null) {
+//                    ans = ans + 0;
+                } else {
+                    int num = tn.left == null ? tn.right.val : tn.left.val;
+                    ans = ans + num;
+                }
+                if (tn.left != null) {
+                    queue.offer(tn.left);
+                }
+                if (tn.right != null) {
+                    queue.offer(tn.right);
+                }
+            }
+        }
+        return ans;
+    }
 }
