4e48b582d066596a97010bc00fb33504e7f491c4
cuizxa <cuizxa@yonyou.com>
2021-11-22 10:37:02 +0800

    update repository

---
+++ src/main/java/com/cuizx/binaryTree/MaxDepth559.java
@@ -0,0 +1,35 @@
+package com.cuizx.binaryTree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MaxDepth559 {
+
+    public int maxDepth(TreeNode root) {
+//        //BFS
+//        Queue<TreeNode> queue = new LinkedList<>();
+//        queue.offer(root);
+//        int ans = 0;
+//        while (!queue.isEmpty()) {
+//            ans++;
+//            int n = queue.size();
+//            for (int i = 0; i < n; i++) {
+//                TreeNode tn = queue.poll();
+//                if (tn.left != null) {
+//                    queue.offer(tn.left);
+//                }
+//                if (tn.right != null) {
+//                    queue.offer(tn.right);
+//                }
+//            }
+//        }
+//        return ans;
+        return 0;
+    }
+
+//    public int dfs(TreeNode treeNode, int ans) {
+//        if (treeNode == null) {
+//            return ans;
+//        }
+//    }
+}
